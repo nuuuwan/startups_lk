d37524ba9a2c3269a3be727a44bf29483e9e8bf0
nuuuwan <nuuuwan@gmail.com>
2021-09-23 10:03:27 +0530

    [add-more-details-to-readme] Done (2021-09-23-1003)

--- src/startups_lk/summary.py
+++ src/startups_lk/summary.py
@@ -41,37 +41,36 @@ def build_startup_summary(data):
     category_str = ' · '.join(category_list)
 
     founder_info = data['founder_info']
-    founder_info_str_list = []
+    founder_info_str_list = [f'[{url}]({url})']
     email = founder_info.get('email')
-    name = founder_info.get('name')
+    founder_name = founder_info.get('name')
     phone = founder_info.get('phone')
     if email:
         founder_info_str_list.append(f'[{email}](mailto:{email})')
     if phone:
         founder_info_str_list.append(f'[{phone}](tel:{phone})')
-    if name:
-        name_str = name.replace(' ', '+')
+    if founder_name:
+        name_str = founder_name.replace(' ', '+')
         name_url = (
             'https://www.linkedin.com/search/results/people/?'
             + f'keywords={name_str}'
         )
-        founder_info_str_list.append(f'[{name}](mailto:{name_url})')
+        founder_info_str_list.append(f'[{founder_name}](mailto:{name_url})')
     founder_info_str = ' · '.join(founder_info_str_list)
 
     startup_stage = data['startup_stage']
     funding_stage = data['funding_stage']
-    details_str = ' · '.join([startup_stage, funding_stage])
+    details_str = ' · '.join([f'**{startup_stage}**', f'Funding **{funding_stage}**'])
 
     return [
         f'## {name}',
+        f'<img src="{img_url}" alt="{name}" style="height:100px; text-align:left;" />',        
         f'*"{tagline}"*',
         f'Business Registration: **{business_registration_str}**',
         f'**{category_str}**',
-        f'<img src="{img_url}" alt="{name}" style="height:100px;" />',
-        f'{description}',
         f'{details_str}',
-        f'{founder_info_str}',
-        f'[{url}]({url})',
+        f'{description}',
+        f'Contact: {founder_info_str}',
     ]
 
 
